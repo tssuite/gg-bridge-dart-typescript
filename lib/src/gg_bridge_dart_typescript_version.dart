3016f5be567aa42efdfad3b8d64a4fa334d711ae
Göran Hegenberg <goeran@hegenberg-privat.de>
2026-09-02 20:53:48 +0200

    #gg: Finish development of version 1.0.3

--- lib/src/gg_bridge_dart_typescript_version.dart
+++ lib/src/gg_bridge_dart_typescript_version.dart
@@ -9,4 +9,4 @@
 // coverage:ignore-file
 
 /// The version of the `gg_bridge_dart_typescript` package.
-const String ggBridgeDartTypescriptVersion = '1.0.2';
+const String ggBridgeDartTypescriptVersion = '1.0.3';
